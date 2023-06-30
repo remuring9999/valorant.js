6dc0bf9a174765b4862e2d0fc64ae2bebb1e95a3
remuring9999 <mooneda9999@gmail.com>
2023-06-30 19:35:48 +0900

    delete: test code

--- test.js
+++
@@ -1,8 +0,0 @@
-const valorant = require("valorant.js");
-
-valorant.search("혜민이가 ZZANG센 주제에#너무신중함").then((res) => {
-  let puuid = res.pageProps.account.puuid;
-  valorant.get(puuid).then((res) => {
-    console.log(res);
-  });
-});
